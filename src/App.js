c9ae486b5ef0b9392013f1b1274af8ea2de50492
Arminas <arminas.abramavicius@gmail.com>
2018-01-18 13:50:47 +0200

    1.33

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Services from "./components/Services";
 import Contact from "./components/Contact";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div className="App">
       <Header />
       <Switch>
